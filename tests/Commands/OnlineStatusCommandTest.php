49794875f6a7218eaf2e85e193da372f577d2845
Tippin <richard.tippin@gmail.com>
2021-02-19 23:52:28 -0500

    OnlineStatusCommandTest.php passes

---
+++ tests/Commands/OnlineStatusCommandTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace RTippin\MessengerFaker\Tests\Commands;
+
+use RTippin\MessengerFaker\Tests\MessengerFakerTestCase;
+
+class OnlineStatusCommandTest extends MessengerFakerTestCase
+{
+    /** @test */
+    public function status_command_does_not_find_thread()
+    {
+        $this->artisan('messenger:faker:status', [
+            'thread' => 404,
+        ])
+            ->expectsOutput('Thread not found.')
+            ->assertExitCode(0);
+    }
+
+    /** @test */
+    public function status_command_group_participants_default_online()
+    {
+        $group = $this->createGroupThread($this->userTippin(), $this->userDoe());
+
+        $this->artisan('messenger:faker:status', [
+            'thread' => $group->id,
+        ])
+            ->expectsOutput('Finished making participants in First Test Group to online!')
+            ->assertExitCode(0);
+    }
+
+    /** @test */
+    public function status_command_private_participants_default_online()
+    {
+        $private = $this->createPrivateThread($this->userTippin(), $this->userDoe());
+
+        $this->artisan('messenger:faker:status', [
+            'thread' => $private->id,
+        ])
+            ->expectsOutput('Finished making participants in Richard Tippin and John Doe to online!')
+            ->assertExitCode(0);
+    }
+
+    /** @test */
+    public function status_command_participants_to_away()
+    {
+        $group = $this->createGroupThread($this->userTippin(), $this->userDoe());
+
+        $this->artisan('messenger:faker:status', [
+            'thread' => $group->id,
+            '--status' => 'away',
+        ])
+            ->expectsOutput('Finished making participants in First Test Group to away!')
+            ->assertExitCode(0);
+    }
+
+    /** @test */
+    public function status_command_participants_to_offline()
+    {
+        $group = $this->createGroupThread($this->userTippin(), $this->userDoe());
+
+        $this->artisan('messenger:faker:status', [
+            'thread' => $group->id,
+            '--status' => 'offline',
+        ])
+            ->expectsOutput('Finished making participants in First Test Group to offline!')
+            ->assertExitCode(0);
+    }
+}
