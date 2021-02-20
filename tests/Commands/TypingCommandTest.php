1fd4472073805f363abfd456db14f1ead2f9bb3a
Tippin <richard.tippin@gmail.com>
2021-02-20 00:02:54 -0500

    TypingCommandTest.php passes

---
+++ tests/Commands/TypingCommandTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace RTippin\MessengerFaker\Tests\Commands;
+
+use RTippin\MessengerFaker\Tests\MessengerFakerTestCase;
+
+class TypingCommandTest extends MessengerFakerTestCase
+{
+    /** @test */
+    public function typing_command_does_not_find_thread()
+    {
+        $this->artisan('messenger:faker:typing', [
+            'thread' => 404,
+        ])
+            ->expectsOutput('Thread not found.')
+            ->assertExitCode(0);
+    }
+
+    /** @test */
+    public function typing_command_group_participants_type()
+    {
+        $group = $this->createGroupThread($this->userTippin(), $this->userDoe());
+
+        $this->artisan('messenger:faker:typing', [
+            'thread' => $group->id,
+        ])
+            ->expectsOutput('Finished making participants in First Test Group type!')
+            ->assertExitCode(0);
+    }
+
+    /** @test */
+    public function typing_command_private_participants_type()
+    {
+        $private = $this->createPrivateThread($this->userTippin(), $this->userDoe());
+
+        $this->artisan('messenger:faker:typing', [
+            'thread' => $private->id,
+        ])
+            ->expectsOutput('Finished making participants in Richard Tippin and John Doe type!')
+            ->assertExitCode(0);
+    }
+}
